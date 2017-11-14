a135dafe7d875827a7c322672198da3053a143e2
Veeramaninatarajanmca <veeramaninatarajanmca@gmail.com>
2017-11-14 18:45:35 +0530

    Importing libraries and Data

--- DigitsRecognition.py
+++ DigitsRecognition.py
@@ -6,4 +6,23 @@ Created on Tue Nov 14 12:22:34 2017
 @Competiion: Analytics Vidhya - Digit Recognition
 
 """
+# Loading required libraries
 
+# To save trained objects
+from sklearn.externals import joblib
+# from sklearn import datasets
+from skimage.feature import hog
+from sklearn.svm import LinearSVC
+import numpy as np
+from scipy import misc
+import glob
+
+# Reading all train images
+features=[]
+for image_path in glob.glob("D:/temp/img/*.png"):
+    image = misc.imread(image_path)
+    features.append(image)
+    print (image.shape)
+    print (image.dtype)
+    
+    
