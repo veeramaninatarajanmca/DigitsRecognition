a3682e4b67e0a4e4f09aed7a5cf5f398f7e5a60e
Veeramaninatarajanmca <veeramaninatarajanmca@gmail.com>
2017-11-21 12:56:48 +0530

    CPB & PPB - Hog feature tuned

--- DigitsRecognition.py
+++ DigitsRecognition.py
@@ -39,7 +39,7 @@ for fn in fname:
 
 list_hog_fd = []
 for feature in features:
-    fd = hog(feature.reshape((28, 28)), orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
+    fd = hog(feature.reshape((28, 28)), orientations=9, pixels_per_cell=(8,8), cells_per_block=(2, 2), visualise=False)
     list_hog_fd.append(fd)
 hog_features = np.array(list_hog_fd, 'float64')
 traindf=pd.DataFrame(hog_features)
@@ -62,7 +62,7 @@ clf.fit(tr_images, tr_labels)
 
 # internal testing - 20% of the data
 test20_pred=clf.predict(te_images)
-test20_pred=pd.Series(test20_pred,dtype="category")ss
+test20_pred=pd.Series(test20_pred,dtype="category")
 test20_act=pd.Series(te_labels,dtype="category")
 accuracy_score(test20_act,test20_pred,normalize=True)
 
@@ -70,22 +70,22 @@ accuracy_score(test20_act,test20_pred,normalize=True)
 
 # Reading test images
 #############################################################################################################
-t1=[]
+t_timage=[]
 test_fname=pd.read_csv("C:\\Users\\user\\Dropbox\\temp\\Hackathons\\AV\\DigitsRecognition\\data\\Test.csv","r")
-tfname=test_fname['filename']
+test_fname=test_fname['filename']
 
 testpath="C:\\Users\\user\\Dropbox\\temp\\Hackathons\\AV\\DigitsRecognition\\data\\Train\\Images\\test\\"
-for fnam in tfname:
-    print(testpath+fnam)
+for fnam in test_fname:
+#    print(testpath+fnam)
     image = misc.imread(testpath+fnam,flatten=True)
-    t1.append(image)
-    print (image.shape)
-    print (image.dtype)
+    t_timage.append(image)
+#    print (image.shape)
+#    print (image.dtype)
     
 
 test_fd= []
-for feat in t1:
-    fd = hog(feat.reshape((28, 28)), orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
+for feat in t_timage:
+    fd = hog(feat.reshape((28, 28)), orientations=9, pixels_per_cell=(8,8), cells_per_block=(2, 2), visualise=False)
     test_fd.append(fd)
 tf = np.array(test_fd, 'float64')
 
@@ -93,12 +93,11 @@ tf = np.array(test_fd, 'float64')
 
 mypred=clf.predict(tf)
 
-mydf2=(pd.Series(mypred))
-test_name=tfname
-test_res=mydf2
+mypred=(pd.Series(mypred))
 
-myresults=pd.concat([test_name,test_res],axis=1)
+
+myresults=pd.concat([test_fname,mypred],axis=1)
 myresults.columns=['filename', 'label']
-myresults.to_csv("d:\\myresults.csv",index=False)
+myresults.to_csv("C:\\Users\\user\\Dropbox\\temp\\Hackathons\\AV\\DigitsRecognition\\output\\myresults4.csv",index=False)
 
 
